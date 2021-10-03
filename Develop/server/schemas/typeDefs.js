41cd3074df2328f033c2d01f0147a7de5676ccf5
janayAl <jsallen298705@gmail.com>
2021-10-02 21:27:48 -0500

    added type user in typedefs file

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -2,7 +2,17 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 
 type Book {
-    _id:ID
+    _id:ID!
     bookId: String
     authors: String
 }
+
+type User{
+    _id: ID!
+    username: String
+    email: String
+    
+
+}
+
+module.exports = typeDefs;
